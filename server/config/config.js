d0db3b11520f81c1acbaac377dd706b8db449acc
Christopher Aedo <Christopher.aedo.p@gmail.com>
2018-04-22 18:41:14 -0300

    rest server usuario

--- server/config/config.js
+++ server/config/config.js
@@ -3,3 +3,25 @@
 /// =============
 
 process.env.PORT = process.env.PORT || 3000;
+
+
+/// =============
+/// entorno
+/// =============
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+
+
+/// =============
+/// base de datos
+/// =============
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = `mongodb://localhost:27017/cafe`;
+} else {
+    urlDB = `mongodb://cafe-user:123456@ds151433.mlab.com:51433/cafe`;
+}
+
+process.env.URLDB = urlDB;
